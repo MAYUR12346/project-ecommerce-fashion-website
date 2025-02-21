e0aabfaf4f4f225f72d3521f8395decf5dedd132
MAYUR123456 <mayurkadammsk380@gmail.com>
2025-02-22 03:11:08 +0530

    made a chenges in index , style file also update images

--- app.py
+++ app.py
@@ -12,9 +12,37 @@ def index():
     return render_template("index.html")
     #return "<p>Hello, World!</p>"
 
-@app.route("/")
+@app.route('/women')
 def women():
-    return render_template("women.html")
+    return render_template('women.html')
+
+@app.route('/men')
+def men():
+    return render_template('men.html')
+
+@app.route('/kids')
+def kids():
+    return render_template('kids.html')
+
+@app.route('/Accessories')
+def Accessories():
+    return render_template('Accessories.html')
+
+@app.route('/Shoes')
+def Shoes():
+    return render_template('Shoes.html')
+
+@app.route('/wishlist')
+def wishlist():
+    return render_template('wishlist.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/Account')
+def Account():
+    return render_template('Account.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
